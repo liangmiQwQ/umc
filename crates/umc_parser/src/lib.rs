bbc70417f4e962d777be128853e1f3ae3bd52939
liangmiQwQ <github@liangmi.dev>
2025-11-16 21:07:18 +0800

    feat: init allocator

--- crates/umc_parser/src/lib.rs
+++ crates/umc_parser/src/lib.rs
@@ -1,14 +1,1 @@
-pub fn add(left: u64, right: u64) -> u64 {
-  left + right
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn it_works() {
-    let result = add(3, 2);
-    assert_eq!(result, 5);
-  }
-}
+pub struct Parser;
