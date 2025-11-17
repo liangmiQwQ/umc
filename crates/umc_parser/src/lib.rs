138076ed7998387b07376b70357ff790ee76fb30
liangmiQwQ <github@liangmi.dev>
2025-11-17 20:55:39 +0800

    chore: init

--- crates/umc_parser/src/lib.rs
+++ crates/umc_parser/src/lib.rs
@@ -1,1 +1,25 @@
-pub struct Parser;
+use oxc_allocator::Allocator;
+
+pub struct Parser<'a> {
+  allocator: &'a Allocator,
+  source_text: &'a str,
+}
+
+impl<'a> Parser<'a> {
+  pub fn new(allocator: &'a Allocator, source_text: &'a str) -> Self {
+    Self {
+      allocator,
+      source_text,
+    }
+  }
+}
+
+#[cfg(test)]
+mod test {
+
+  #[test]
+  fn test_create_parser() {
+    let allocator = Allocator::default();
+    let parser = Parser::new(&allocator, "some code there");
+  }
+}
