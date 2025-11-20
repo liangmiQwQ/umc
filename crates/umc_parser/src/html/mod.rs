d0a211707475a9f0632815496eaab606288981fb
liangmiQwQ <github@liangmi.dev>
2025-11-20 20:12:46 +0800

    chore: update

--- crates/umc_parser/src/html/mod.rs
+++ crates/umc_parser/src/html/mod.rs
@@ -1,15 +1,23 @@
 use crate::Parser;
 use oxc_parser::ParseOptions;
 
+pub enum HtmlType {
+  Auto, // Read the !DOCTYPE tag, use Html5 by default
+  XHtml,
+  Html5,
+}
+
 pub struct HtmlParserOptions {
   // If get None, the content in <script> tag will be returned without parsing
   parse_script: Option<ParseOptions>,
+  html_type: HtmlType,
 }
 
 impl Default for HtmlParserOptions {
   fn default() -> Self {
     HtmlParserOptions {
       parse_script: Some(ParseOptions::default()),
+      html_type: HtmlType::Auto,
     }
   }
 }
