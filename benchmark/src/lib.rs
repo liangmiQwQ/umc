fe9a2054ef8ca69168dc0a664ff6710d05fa2514
liangmiQwQ <github@liangmi.dev>
2025-12-13 22:13:13 +0800

    bench:

--- benchmark/src/lib.rs
+++ benchmark/src/lib.rs
@@ -1,19 +1,32 @@
+use std::hint::black_box;
+
 use criterion::{Criterion, criterion_group, criterion_main};
+use oxc_allocator::Allocator;
+use tokio::runtime::Runtime;
+use umc_html_parser::CreateHtml;
+use umc_parser::Parser;
 
-async fn bench_html(c: &mut Criterion) {
+fn bench_html(c: &mut Criterion) {
   // get html from https://www.wikipedia.org/
-  let html = reqwest::get("https://www.wikipedia.org/")
-    .await
-    .unwrap()
-    .text()
-    .await
-    .unwrap();
+  let rt = Runtime::new().unwrap();
+  let html = rt.block_on(async {
+    reqwest::get("https://www.wikipedia.org/")
+      .await
+      .unwrap()
+      .text()
+      .await
+      .unwrap()
+  });
 
-  c.bench_function("html parse", |b| {});
-}
+  c.bench_function("html parse", |b| {
+    let allocator = Allocator::new();
+    let parser = Parser::html(&allocator, &html);
 
-fn bench() {
-  let allocator = Allocator::new();
+    b.iter(|| {
+      let result = parser.parse();
+      black_box(result)
+    });
+  });
 }
 
 criterion_group!(benches, bench_html);
